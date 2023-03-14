700750e42e01e8acc3d180a818dfa91a6789e206
stephaneheck <stephane.heck75@gmail.com>
2023-03-14 08:57:37 +0100

    REST API call using axios, search filter on the retrieved data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
+/** REACT - Hooks **/
+import React, { useState, useEffect } from 'react'
+
+/** Axios - handles REST API calls **/
+import axios from "axios";
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+	/* create REST client instance */
+	const client = axios.create({ baseURL: "https://jsonplaceholder.typicode.com/posts" });
+
+	/** the data of the REST API call */
+	const [posts, setPosts] = useState([]);
+
+	// the value of the input search field 
+	const [name, setName] = useState('');
+	
+	// the search result
+	const [foundInterfaces, setFoundInterfaces] = useState([]);
+	
+	/** Search Filter on the gathered REST API call**/
+	const filter = (e) => {
+		const keyword = e.target.value;
+
+		if (keyword !== '') {
+			// filter on the complete population
+			const results = posts.filter((interfaceItem) => {
+			return ( 
+				// search in title and body, avoid case sensitive
+					interfaceItem.title.toLowerCase().includes(keyword.toLowerCase()) ||
+					interfaceItem.body.toLowerCase().includes(keyword.toLowerCase())
+				);
+		  });
+		  	setFoundInterfaces(results); // update the list of interfaces
+		} else {
+			setFoundInterfaces(posts); // If search field is empty, show all
+		}
+		setName(keyword);
+	  };
+
+	  // GET with Axios
+	  useEffect(() => {
+		  const fetchPost = async () => {
+			 let response = await client.get('?_limit=100');
+			 setPosts(response.data);
+			 setFoundInterfaces(response.data);
+		  };
+		  fetchPost();
+	   }, []);	
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +67,32 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div className="container">
+			<input type="search" value={name} onChange={filter} className="input" placeholder="Filter"/>
+
+			<table border="1">
+				<thead>
+					<tr className="interface-row-header">
+						<th className="">API Name</th>
+						<th className="">Description</th>
+						<th className="">Type</th>
+					</tr>
+				</thead>
+				<tbody>
+			  {foundInterfaces.map((interfaceItem) => {
+				 return (
+					<tr key={interfaceItem.id} className="interface-row">
+						<td className="interface-name">{interfaceItem.title}</td>
+						<td className="interface-description">{interfaceItem.body}</td>
+						<td className="interface-type">{interfaceItem.userId}</td>
+					</tr>
+				 );
+			  })}
+				</tbody>
+			</table>
+		</div>
+
     </div>
   );
 }
